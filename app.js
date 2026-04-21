d71b981d8d1ac37c958c7e8eed6185c1001e60fb
ManavPandya31 <manavpandya42@gmail.com>
2026-04-21 12:42:34 +0530

    update

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 // }));
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://chat-application-frontend-zeta.vercel.app/"
+  "https://chat-application-by-manavpandya.onrender.com"
 ];
 
 app.use(cors({
